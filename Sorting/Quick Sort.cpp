6afe49670e8e2d8ba4c53008103253399ddbf94e
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-02-15 20:45:12 +0530

    Create Quick Sort.cpp

---
+++ Sorting/Quick Sort.cpp
@@ -0,0 +1,56 @@
+/* C++ implementation of QuickSort */
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high];
+	int i
+		= (low
+		- 1); 
+	for (int j = low; j <= high - 1; j++) {
+		if (arr[j] < pivot) {
+			i++; 
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high) {
+		int pi = partition(arr, low, high);
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+int main(){
+    int i,n;
+    cout<<"enter the limit of an array";
+    cin>>n;
+    int arr[n];
+    cout<<"enter elements to array";
+    for (i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"array before sorting\n";
+    for (i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    quickSort(arr,0,n);
+    cout<<"\narray after sorting\n";
+    for (i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
