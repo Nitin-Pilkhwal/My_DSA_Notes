5dc7af508ff060238b4a70d3d47cead34b5189b2
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-03-09 23:13:46 +0530

    Create Postorder-traversal.cpp

---
+++ Tree/Postorder-traversal.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//Creating Node
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node (int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder (struct Node* root){
+    if(root == NULL) return ;
+    preorder(root->left);
+    preorder(root->right);
+    cout<<root->data<<" ";
+}
+
+int main(){
+    struct Node * root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left = new Node(4);
+    root->right = new Node(5);
+
+    /*
+                   1
+                  / \
+                 2   3 
+                / \
+               4   5 
+    */
+    preorder(root);
+    return 0;
+}
