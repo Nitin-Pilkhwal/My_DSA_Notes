1154085bafee3a5f315b6db73e663de331a48403
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-06-08 22:06:32 +0530

    Update Quick Sort.cpp

--- Sorting/Quick Sort.cpp
+++ Sorting/Quick Sort.cpp
@@ -12,9 +12,7 @@ void swap(int* a, int* b)
 int partition(int arr[], int low, int high)
 {
 	int pivot = arr[high];
-	int i
-		= (low
-		- 1); 
+	int i = (low- 1); 
 	for (int j = low; j <= high - 1; j++) {
 		if (arr[j] < pivot) {
 			i++; 
